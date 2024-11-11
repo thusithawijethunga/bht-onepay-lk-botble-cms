947d8c7c0dbddb29ccfc1a73a3bd583ea2387cd0
Thusitha Wijethunga <thusithawijethunga@gmail.com>
2024-11-11 11:24:16 +0530

    feat: create settings.blade.php

---
+++ resources/views/settings.blade.php
@@ -0,0 +1,27 @@
+<ol>
+    <li>
+        <p> Create an OnePay Merchant Account
+            <a href="https://merchant-v2.onepay.lk/" target="_blank">
+              more info Merchant OnepayLk
+            </a>
+        </p>
+        <p>After Creating  Merchant Account goto this url for
+            <a href="https://merchant-v2.onepay.lk/pages/developer-configurations/ipg-apps" target="_blank">
+                IPG Apps Details
+            </a>
+        </p>
+    </li>
+    <li>
+        <p>After sign up. Login in. create ipg app then get you</p>
+        <ul>
+            <li>App ID</li>
+            <li>Hash Salt</li>
+            <li>App Token</li>
+            <li>Callback Token</li>
+        </ul>
+    </li>
+    <li class="pt-4">
+        <p>{{ __('Enter all into into the box in right hand') }}</p>
+    </li>
+</ol>
+
