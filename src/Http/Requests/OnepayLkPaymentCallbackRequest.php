e490b0cd9c8ccef8d751e7bf261b8859445eb774
Thusitha Wijethunga <thusithawijethunga@gmail.com>
2024-11-11 11:24:48 +0530

    feat: create OnepayLkPaymentCallbackRequest.php

---
+++ src/Http/Requests/OnepayLkPaymentCallbackRequest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ByHeartLK\OnepayLk\Http\Requests;
+
+use Botble\Support\Http\Requests\Request;
+
+class OnepayLkPaymentCallbackRequest extends Request
+{
+    public function rules(): array
+    {
+        return [];
+    }
+}
